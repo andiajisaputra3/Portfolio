6898f04e0280cca306b51c91f2188d22d5ef087e
aandy217 <andiajisaputra3@gmail.com>
2024-07-28 10:45:17 +0700

    four commit

--- src/components/Contact/ContactDetail.jsx
+++ src/components/Contact/ContactDetail.jsx
@@ -9,7 +9,7 @@ export default function ContactDetail() {
     return (
         <div className='w-full px-14 lg:px-20 py-10 bg-white shadow-custom-dark lg:shadow-none'>
             <h1 className='font-poor mb-3 text-6xl text-center lg:text-start lg:text-7xl'>Let's Talk</h1>
-            <p className='text-center'>I’m excited to hear from you! Drop me a message and let’s create something great together.</p>
+            <p className='text-center lg:text-start'>I’m excited to hear from you! Drop me a message and let’s create something great together.</p>
             <div className='mt-10'>
                 <div className='flex items-center gap-3 py-4 '>
                     <p><MdEmail className='text-3xl' /></p>

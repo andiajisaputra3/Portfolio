e8f40fb569bf3670a2cfcc792e0e04271072354b
aandy217 <andiajisaputra3@gmail.com>
2024-07-28 10:00:31 +0700

    second commit

--- src/components/Contact/ButtonCv.jsx
+++ src/components/Contact/ButtonCv.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ButtonCv() {
     return (
         <div className='mt-7 w-full lg:w-44 h-12 '>
-            <a href="../../../public/assets/CV.pdf" download="CV_Andi_Aji_Saputra.pdf" className='link-cv group relative block w-full h-full text-center items-center content-center border-2 border-customPrimary font-semibold rounded-md overflow-hidden transition duration-500 cursor-pointer outline-none hover:border-white'>
+            <a href="/assets/CV.pdf" download="CV_Andi_Aji_Saputra.pdf" className='link-cv group relative block w-full h-full text-center items-center content-center border-2 border-customPrimary font-semibold rounded-md overflow-hidden transition duration-500 cursor-pointer outline-none hover:border-white'>
                 <button type="button" className='btn-cv relative transition duration-500 group-hover:text-white'>Download CV</button>
             </a>
         </div>

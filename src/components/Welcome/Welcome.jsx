60eebdf30b29479308645897e4659d8273dbae21
aandy217 <andiajisaputra3@gmail.com>
2024-08-03 10:53:00 +0700

    Fina

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Welcome() {
     return (
         <div id='intro-welcome' className='fixed top-0 left-0 w-full h-full px-5 bg-customThird flex flex-col justify-center place-items-center items-center text-center z-50'>
-            <h1 id='title-1' className="text-customSecond text-6xl lg:text-9xl font-poor">Welcom</h1>
+            <h1 id='title-1' className="text-customSecond text-6xl lg:text-9xl font-poor">Welcome</h1>
             <h1 id='title-1' className="text-white text-6xl lg:text-9xl font-poor">To My Portfolio</h1>
         </div>
     )
